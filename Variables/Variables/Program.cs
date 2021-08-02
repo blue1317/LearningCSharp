da53e99d917179d6b3a073665faf459ca43e1eb9
Chris <52719722+blue1317@users.noreply.github.com>
2021-08-01 20:46:59 -0400

    Section 3
    Primitive types and Expressions
    C# basics for beginners

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 29;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'C';
+            string firstName = "Chris";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}

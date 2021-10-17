b5ac2a739332665d58fe77f0a5cd07002d6ad484
Chris <52719722+blue1317@users.noreply.github.com>
2021-10-17 16:48:55 -0400

    inheritance and composition

---
+++ Mosh CSharp Intermediate/Inheritance/Inheritance/PersentationObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class PersentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object was Copied to clipboard");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was Duplicated");
+        }
+    }
+}
---
+++ Mosh CSharp Intermediate/Inheritance/Inheritance/Text.cs
@@ -0,0 +1,13 @@
+﻿namespace Inheritance
+{
+    public class Text : PersentationObject
+    {
+        public int FontSize { get; set; }
+        public int FontName { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            System.Console.WriteLine("We added a link to " + url);
+        }
+    }
+}

e0d13783a8da925d5a589417fcffb71a1fa4493f
Chris <52719722+blue1317@users.noreply.github.com>
2021-10-19 00:19:44 -0400

    u

---
+++ Mosh CSharp Intermediate/Section 4 Inheritance/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AccessModifiers
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            
+        }
+    }
+}
